af39c4f9a304bffb32879e75cea6adb1002a612f
Pocento <cristiancid88@gmail.com>
2021-09-01 02:53:12 -0400

    clase4

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,9 @@
+// importacion de componentes
 import { Component } from '@angular/core';
+// importacion de linkeo entre paginas
 import { Router } from '@angular/router';
+//importacion de Control de Formularios
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +12,10 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
+  // en su interio se declaran los objetos a inyectar en la pagina Typescript
   constructor(private router: Router) {}
 
+  // linkeo entre paginas
   irPagina1(){
     this.router.navigate(['/ejemplo1'])
   }
@@ -19,10 +25,20 @@ export class HomePage {
   irPagina3(){
     this.router.navigate(['/ejemplo3'])
   }
+    // Definicion de variables siempre sera ' nombre: tipo="valor inicial" ' 
+    //puede ser String, boolean, number, any (cualquier dato), Array (definicion de arreglo)
   rut:String ="";
   nombreCompleto:string ="";
-  edad:BigInteger;
+  edad:Number;
+  // declaración de objetos de tipo formulario
+  elrut = new FormControl('');
+  
+
+
+  // Metodos de acceso
   cambiarDatos(){
     alert('pincho boton');
   }
 }
+
+//implementacion de codigo en javascript 
